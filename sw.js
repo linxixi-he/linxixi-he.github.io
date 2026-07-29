89acf7bf396a5533c88eb9c23fd00e3f4362c994
linxixi-he <2062601678@qq.com>
2026-07-29 18:40:23 +0800

    fix sync upsert (via API)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 缓存优先，断网也能用
-const CACHE_NAME = 'linxixi-v10';
+const CACHE_NAME = 'linxixi-v11';
 const ASSETS = ['./', './index.html', './sw.js'];
 
 self.addEventListener('install', e => {
