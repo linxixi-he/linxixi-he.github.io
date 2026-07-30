feffc823b491d3edcbc2ba6009fa99fd79e4da6a
linxixi-he <2062601678@qq.com>
2026-07-30 20:03:34 +0800

    fix: stable task IDs + smart dedup + proxy live-reload (v14)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 缓存优先，断网也能用
-const CACHE_NAME = 'linxixi-v13';
+const CACHE_NAME = 'linxixi-v14';
 const ASSETS = ['./', './index.html', './sw.js'];
 
 self.addEventListener('install', e => {
