e4662a740d64aeaaaf7e5942c52098566135a09b
linxixi-he <2062601678@qq.com>
2026-08-04 17:26:25 +0800

    chore: bump sw to v18 for food panels

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 缓存优先，断网也能用
-const CACHE_NAME = 'linxixi-v16';
+const CACHE_NAME = 'linxixi-v18';
 const ASSETS = ['./', './index.html', './sw.js'];
 
 self.addEventListener('install', e => {
